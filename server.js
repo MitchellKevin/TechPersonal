1f075657913fbdfad7ffc371b1c20097fa2673b7
Mitchell Scholte <mitchell.scholte04@gmail.com>
2025-02-24 15:58:07 +0100

    update

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv= require('dotenv');
+dotenv.config();
 const express= require('express');
 const path= require('path');
 const app = express();
@@ -12,6 +14,30 @@ app.get('/about', about)
 app.get('/movie', movie)
 app.listen(port, () => {
     console.log('Server is running on port 8000');
+});
+
+// const {MongoClient}= require('mongodb');
+// const uri= process.env.URI;
+// const db_name= process.env.DB_NAME;
+
+// const client= new MongoClient(uri);
+// const database= client.db(db_name);
+
+// async function connect() {
+// try {
+//     await client.connect();
+//     console.log('Connected to the database');
+// } catch (error) {
+//     console.log('Error connecting to the database');
+// }
+// }
+
+// connect();
+
+app.post(
+    '/movie', 
+    async (req, res) => {
+    console.log(req.body);
 });
 
 function onhome(req, res) {
